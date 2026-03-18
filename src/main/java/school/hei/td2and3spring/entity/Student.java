671f88945d5bc70337ba6d892d81bf16aa6eab56
jessyrand <henintsoarandriam@gmail.com>
2026-03-18 16:12:56 +0300

    feat(entity): add Student class

---
+++ src/main/java/school/hei/td2and3spring/entity/Student.java
@@ -0,0 +1,8 @@
+package school.hei.td2and3spring.entity;
+
+public class Student {
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+}
