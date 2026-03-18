78fbbe6b9013c253f8d037da3baba5597c40fdcd
jessyrand <henintsoarandriam@gmail.com>
2026-03-18 16:39:28 +0300

    chore(entity): add Student constructor and getters

--- src/main/java/school/hei/td2and3spring/entity/Student.java
+++ src/main/java/school/hei/td2and3spring/entity/Student.java
@@ -1,5 +1,12 @@
 package school.hei.td2and3spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Student {
     private String reference;
     private String firstName;
