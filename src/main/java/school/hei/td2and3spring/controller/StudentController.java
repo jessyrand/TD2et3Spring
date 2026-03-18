e857c6069fa80267f77b8a5bb624261792cc4f10
jessyrand <henintsoarandriam@gmail.com>
2026-03-18 16:18:30 +0300

    chore(controller): add Restcontroller annotation, welcome endpoint and inject StudentService

--- src/main/java/school/hei/td2and3spring/controller/StudentController.java
+++ src/main/java/school/hei/td2and3spring/controller/StudentController.java
@@ -1,4 +1,17 @@
 package school.hei.td2and3spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.td2and3spring.service.StudentService;
+
+@RestController
 public class StudentController {
+
+    private StudentService studentService;
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam String name){
+        return "Welcome "+name+"!";
+    }
 }
