b28965202db302bd8a7020e1bb486769c98329ac
jessyrand <henintsoarandriam@gmail.com>
2026-03-18 16:09:52 +0300

    initial commit

---
+++ src/main/java/school/hei/td2and3spring/Td2and3SpringApplication.java
@@ -0,0 +1,13 @@
+package school.hei.td2and3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td2and3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td2and3SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/td2and3spring/Td2and3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.td2and3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td2and3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
