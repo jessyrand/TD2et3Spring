0425ed894b8d71077082deacd11d66a6d3fabd34
jessyrand <henintsoarandriam@gmail.com>
2026-03-18 16:14:16 +0300

    feat(service): add StudentService class

---
+++ src/main/java/school/hei/td2and3spring/service/StudentService.java
@@ -0,0 +1,21 @@
+package school.hei.td2and3spring.service;
+
+import org.springframework.stereotype.Service;
+import school.hei.td2and3spring.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+    private List<Student> students =  new ArrayList<>();
+
+    public List<Student> getALl(){
+        return students;
+    }
+
+    public List<Student> save(Student student){
+        students.add(student);
+        return students;
+    }
+}
